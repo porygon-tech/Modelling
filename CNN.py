70b396f36cd083a4ef98204251a9cb17e7eb1850
porygon-tech <59631625+porygon-tech@users.noreply.github.com>
2020-10-08 17:37:22 +0200

    Create CNN.py

---
+++ CNN.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import numpy as np
