477cd0c03858219ac9bc6bc3a997434e25549dc7
porygon-tech <59631625+porygon-tech@users.noreply.github.com>
2020-11-26 14:56:13 +0100

    Update CNN.py

--- RI_delivery_1/CNN.py
+++ RI_delivery_1/CNN.py
@@ -5,8 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-#https://www.codingforentrepreneurs.com/blog/install-tensorflow-gpu-windows-cuda-cudnn/
+
 #=== FUNCTIONS ====
+
 def unison_shuffle(a, b):
     assert len(a) == len(b)
     p = np.random.permutation(len(a))
@@ -23,9 +24,9 @@ def showdata(mat, index):
 	plt.show()
 
 
-#===============
-#=== SCRIPT ====
-#===============
+
+#=== DATA LOADING ====
+
 csvpath = '/home/mroman/Master/images_train/'
 filepath = csvpath + 'images/'
 
@@ -34,7 +35,9 @@ labels = df[1:,1]
 imgIDs = df[1:,0]
 
 
+
 #=== LABEL FORMATTING ===
+
 dictID = {}
 IDcounter = 0
 y_num = []
@@ -50,7 +53,9 @@ ty = np.array(y_num)[:,np.newaxis]
 num_classes = len(dictID)
 
 
+
 #=== FEATURES FORMATTING ===
+
 imageSet = []
 for imgID in imgIDs:
 	filename = str(filepath + "train_image_" + imgID.replace('\"', '') + ".png")
@@ -62,35 +67,37 @@ tx = (np.array(imageSet)[:,0,0,:,:,:] / 255).astype('float16')
 
 
 
-
-
 #=== DATA SPLIT ===
-'''
-a: data   for training
-b: labels for training
-c: data   for testing
-d: labels for testing
-'''
+
+#	a: data   for training
+#	b: labels for training
+#	c: data   for testing
+#	d: labels for testing
+
 randtx, randty = unison_shuffle(tx,ty)
 a,b,c,d = splitdata(randtx,randty,0.2)
-'''
+
+''' 
+# Useful chunks for saving and loading data (for reproducibility reasons)
 np.save(filepath + "a", a)
 np.save(filepath + "b", b)
 np.save(filepath + "c", c)
 np.save(filepath + "d", d)
-'''
 
 a = np.load(filepath + "a.npy")
 b = np.load(filepath + "b.npy")
 c = np.load(filepath + "c.npy")
 d = np.load(filepath + "d.npy")
+'''
+
 
 #=== MODEL BUILDING ===
+
 num_train, img_rows, img_cols, img_channels =  tx.shape
 
 model = Sequential()
-model.add(Conv2D(filters=img_rows, kernel_size=(1, 3), padding='same', activation='relu', input_shape = [img_rows, img_cols, img_channels]))
-model.add(Conv2D(filters=img_rows, kernel_size=(3, 1), padding='same', activation='relu'))
+model.add(Conv2D(filters=128, kernel_size=(1, 3), padding='same', activation='relu', input_shape = [img_rows, img_cols, img_channels]))
+model.add(Conv2D(filters=128, kernel_size=(3, 1), padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2), strides=2, padding='valid'))
 model.add(Dropout(0.5))
 model.add(Flatten())
@@ -102,31 +109,30 @@ model.compile(optimizer='adam', loss = 'sparse_categorical_crossentropy', metric
 
 
 
+#=== PLAYGROUND ===
+
+#DO NOT TRY BOTH OPTIONS WITHOUT RESETTING MODEL WEIGHTS BEFORE
 
 
-
+#option A: training with shuffled, split data
 
 model.evaluate(c, d, batch_size=32)
+#(without training, accuracy will be quite low)
 
-#model.load_weights(checkpoint_path)
-
-
-checkpoint_path = csvpath + "haha_1.ckpt"
+checkpoint_path = csvpath + "split.ckpt"
 cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=True, verbose=1)
 
 model.fit(a, b, batch_size=32, epochs=10, verbose=1, callbacks=[cp_callback])
+'''
+# if the model has been previously trained, the line below can be uncommented to load the trained weights
+model.load_weights(checkpoint_path)
+'''
 
-#history = model.fit(tx,ty, batch_size=32, epochs=10, verbose=1, callbacks=[cp_callback])
-
+#below are some
 model.evaluate(c, d, batch_size=32)
-
-
-
-
 
 for i in dictID.keys():
 	print(i, dictID[i])
-
 
 p = model.predict(c)
 
@@ -134,42 +140,12 @@ plt.imshow(p, interpolation='none', cmap='inferno')
 plt.show()
 
 
-
-
-
-
+#option B: training using the full dataset (without testing)
 
 checkpoint_path = csvpath + "fulltrained.ckpt"
 cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=True, verbose=1)
+
 model.fit(tx,ty, batch_size=32, epochs=10, verbose=1, callbacks=[cp_callback])
-
-
-
-
-
-
-
-
-
-#=== PLAYGROUND ===
-
-
-def unison_shuffle(a, b):
-    assert len(a) == len(b)
-    p = np.random.permutation(len(a))
-    return a[p], b[p]
-
-randtx, randty = unison_shuffle(tx,ty)
-
-import math
-def splitdata(a, b, fraction):
-	assert len(a) == len(b)
-	length = len(a)
-	train_num = math.floor(length * (1-fraction))
-	return(a[0:train_num],b[0:train_num],a[train_num:],b[train_num:])
-
-a,b,c,d = splitdata(tx,ty,0.2)
-
 
 
 showdata(tx,5)
