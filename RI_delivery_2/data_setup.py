42c8e1dc56f013a5a41211b27f0c5feaf5a589ee
porygon-tech <59631625+porygon-tech@users.noreply.github.com>
2020-12-10 23:12:41 +0100

    Update data_setup.py

--- RI_delivery_2/data_setup.py
+++ RI_delivery_2/data_setup.py
@@ -1,12 +1,25 @@
 import sqlite3
 import pandas as pd
 import numpy as np
+import math 
 
 def encode(A):
 	classes = np.unique(A)
 	dictionary = dict(zip(classes, range(len(classes))))
 	A_numeric = list(map(dictionary.get, A))
 	return(A_numeric, dictionary)
+
+def unison_shuffle(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
+
+def splitdata(a, b, fraction):
+	assert len(a) == len(b)
+	length = len(a)
+	train_num = math.floor(length * (1-fraction))
+	return(a[0:train_num],b[0:train_num],a[train_num:],b[train_num:])
+
 
 
 filepath = '/home/mroman/Master/RI_deliv_2/gdp_growth_forecast/'
@@ -17,15 +30,41 @@ df = pd.read_sql_query('SELECT c.CountryCode, i.IndicatorCode, ci.Year, ci.Value
 test = df.pivot_table(index=['CountryCode','Year'], columns="IndicatorCode", values='Value')
 test['nextyearGDPgrowth'] = test['NY.GDP.MKTP.KD.ZG'].shift(periods=-1)
 
-#test.to_sql("formatted", con, if_exists="replace")
 
-classList = np.array(list(map(list, test.index)))
+def fixvalue(value, fixedvalue, isNA=False):
+	if isNA:
+		return(fixedvalue)
+	else:
+		return(value)
+
+def fixcol(col):
+	mu = np.mean(col)
+	sd = np.std(col)
+	length = len(col)
+	fixedvalues = np.random.normal(mu, sd, length)
+	NAs = col.isin(['NaN'])
+	return list(map(fixvalue, col,fixedvalues,NAs))
+
+def fixtab(tab):
+	for i in tab.columns:
+		tab[i] = fixcol(tab[i])
+	return tab
+
+
+filtered = test.dropna(thresh=2/3*test.shape[0], axis=1)
+filtered = fixtab(filtered)
+
+
+
+
+
+classList = np.array(list(map(list, filtered.index)))
 
 countryCodes_num, countryDict = encode(classList[:,0])
 years_num = np.array(list(map(int, classList[:,1])))[:,np.newaxis]
 countryCodes_num = np.array(countryCodes_num)[:,np.newaxis]
 
-values = test.to_numpy()
+values = filtered.to_numpy()
 dataset = np.concatenate((countryCodes_num,years_num,values), axis=1)
 '''
 dataset[:,0] #numeric country code
